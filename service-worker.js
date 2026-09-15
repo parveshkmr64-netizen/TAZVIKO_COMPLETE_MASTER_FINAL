f58b7515ef71208dda3dcf833ba33a77495abdac
parveshkmr64-netizen <parveshkmr64@gmail.com>
2026-09-15 12:41:59 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='tazviko-v4-marketplace-final';
+const CACHE='tazviko-v5-partner-form-hotfix';
 const CORE=['./','./index.html','./partner.html','./rider.html','./admin.html','./offline.html','./manifest.webmanifest','./js/app-config.js','./js/api-client.js','./js/launch-ready.js','./js/pwa.js','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));

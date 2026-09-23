a1c7604b803df23c42a223ef272f60621d181016
parveshkmr64-netizen <parveshkmr64@gmail.com>
2026-09-23 18:18:51 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // fallback-cards-v2
-const CACHE='tazviko-premium-v8-fallback';
+const CACHE='tazviko-premium-v3-visible-cards';
 const CORE=['./','./index.html','./offline.html','./manifest.webmanifest','./js/app-config.js','./js/api-client.js','./js/launch-ready.js','./js/pwa.js','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
